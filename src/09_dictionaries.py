3959eaea04edf8e0ce18ef7139a12d0b72774533
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-01-02 15:27:04 -0800

    more done

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,10 +35,24 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+myDict = dict([('lat', 12), ('lon', 13), ('name', 'yet another place')])
+waypoints.append(myDict)
+
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+updateDict = dict([('lat', 43), ('lon', -130), ('name', 'not a real place')])
+waypoints[0] = updateDict
+
+print()
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+print()
+for i in waypoints:
+  for key, value in i.items() :
+    print (key, value)
+  
